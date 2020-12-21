2080ec76f118e214b12fc61b2a392da796716f95
eva-barczykowska <eva.barczykowska@gmail.com>
2020-12-21 11:54:57 +0100

    changed numbers for the code to work

--- make_call.rb
+++ make_call.rb
@@ -2,14 +2,14 @@ require 'twilio-ruby'
 
 # Get your Account Sid and Auth Token from twilio.com/console
 account_sid = 'AC4847f9ef53169ffe5d22949105436e36'
-auth_token = '749552fdb07f8b111b404a39fc94a666'
+auth_token = 'd89487f8d163d891d7d1aa736d65193e'
 
 # set up a client to talk to the Twilio REST API
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 
 call = @client.calls.create(
-    to: "[+][49][15152279541]",
-    from: "[+][49][15152213920]",
+    to: "[+][49][15152213920]",
+    from: "[+][49][15152279541]",
     url: "http://demo.twilio.com/docs/voice.xml")
 
 puts call.to
--- quickstart.rb
+++ quickstart.rb
@@ -14,14 +14,14 @@
 #
 # end
 
-
-require 'sinatra'
-require 'twilio-ruby'
-
-get '/' do
-  content_type 'text/xml'
-
-  Twilio::TwiML::VoiceResponse.new do | response |
-    response.say(message: "Hello World")
-  end.to_s
-end
+# 
+# require 'sinatra'
+# require 'twilio-ruby'
+#
+# get '/' do
+#   content_type 'text/xml'
+#
+#   Twilio::TwiML::VoiceResponse.new do | response |
+#     response.say(message: "Hello World")
+#   end.to_s
+# end
