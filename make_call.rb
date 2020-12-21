5c9ec68b2e64a0f85044213c3a8584b79b9400f6
eva-barczykowska <eva.barczykowska@gmail.com>
2020-12-21 12:28:33 +0100

    removed twilio sid and auth

--- make_call.rb
+++ make_call.rb
@@ -1,8 +1,8 @@
 require 'twilio-ruby'
 
 # Get your Account Sid and Auth Token from twilio.com/console
-account_sid = 'AC4847f9ef53169ffe5d22949105436e36'
-auth_token = 'd89487f8d163d891d7d1aa736d65193e'
+account_sid = ''
+auth_token = ''
 
 # set up a client to talk to the Twilio REST API
 @client = Twilio::REST::Client.new(account_sid, auth_token)
