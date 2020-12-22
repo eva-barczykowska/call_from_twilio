e8584922980ffbee0dcfdafe4d0981f56a5fdc15
eva-barczykowska <eva.barczykowska@gmail.com>
2020-12-22 16:26:58 +0100

    fixed twilio credentials

--- make_call.rb
+++ make_call.rb
@@ -1,8 +1,8 @@
 require 'twilio-ruby'
 
 # Get your Account Sid and Auth Token from twilio.com/console
-account_sid = ''
-auth_token = ''
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
 
 # set up a client to talk to the Twilio REST API
 @client = Twilio::REST::Client.new(account_sid, auth_token)
